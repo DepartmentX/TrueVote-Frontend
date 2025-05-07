b8d3025e570f1f9a1ae1bbf3ddf5bbe4e27f782d
ShanthishaShyamana <shanthisha2002@gmail.com>
2025-05-07 12:12:11 +0530

    factory address changed

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,4 +4,4 @@ import CampaignFactoryABI from './abis/CampaignFactory.json';
 export const CAMPAIGN_ABI = CampaignABI.abi;
 export const FACTORY_ABI = CampaignFactoryABI.abi;
 
-export const FACTORY_ADDRESS = "0xB5c5b35B206Cc85D6c34742e8e68381ce15a78f4"; // ← paste from deploy
+export const FACTORY_ADDRESS = "0xca8350ec67486241Aa55ee45461a645c067C95a5"; // ← paste from deploy
